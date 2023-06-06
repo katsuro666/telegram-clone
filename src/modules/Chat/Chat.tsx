d03f8ed044fc0738d3c29b7ccdabb5670a07d7d9
katsuro666 <kreepinoutdakut@gmail.com>
2023-06-06 17:55:36 +0300

    add Header to the Chat section

--- src/modules/Chat/Chat.tsx
+++ src/modules/Chat/Chat.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Chat.scss'
+import { Header } from './components'
 
 export function Chat() {
   return (
     <div className='chat--gradient'>
-      <div className="chat"></div>
+      <div className="chat">
+        <Header />
+      </div>
     </div>
   )
 }
