34f67924b8f789c60a650d43adcfc245cfae50d0
katsuro666 <kreepinoutdakut@gmail.com>
2023-06-06 13:07:32 +0300

    add chat section with bg

---
+++ src/modules/Chat/Chat.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Chat.scss'
+
+export function Chat() {
+  return (
+    <div className='chat--gradient'>
+      <div className="chat"></div>
+    </div>
+  )
+}
