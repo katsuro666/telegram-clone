a14eba1a239d3fa84d2832469f7d2cf5a6d82195
katsuro666 <kreepinoutdakut@gmail.com>
2023-06-06 12:04:36 +0300

    add 'create new msg' icon on the sidebar

--- src/modules/Sidebar/Sidebar.tsx
+++ src/modules/Sidebar/Sidebar.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Sidebar.scss';
 import { Header, Threads } from './components';
+import CreateIcon from '@mui/icons-material/Create';
 
 export function Sidebar() {
   return (
     <div className='sidebar'>
       <Header />
       <Threads />
-      <div className="sidebar__newMsg"></div>
+      <div className="sidebar__new-msg">
+        <CreateIcon className="new-msg__icon"/>
+      </div>
     </div>
   )
 }
