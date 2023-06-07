f09c821e675fb7e6e00b9b9b274e4fde6fcda9ff
katsuro666 <kreepinoutdakut@gmail.com>
2023-06-07 15:58:00 +0300

    make Chat Input controlled

--- src/modules/Chat/components/Input/Input.tsx
+++ src/modules/Chat/components/Input/Input.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Input.scss'
-// import MicIcon from '@mui/icons-material/Mic';
 import SendIcon from '@mui/icons-material/Send';
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import { IconButton } from '@mui/material';
 
 export  function Input() {
+
+  const [message, setMessage] = useState('')
+
+  const sendMessage = () => {
+    console.log(message)
+    
+    // firebase stuff
+
+    setMessage('')
+  }
+  
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.key === 'Enter' && sendMessage()
+  }
+
   return (
     <div className="input-outer">
       <div className='chat__input-container'>
@@ -14,13 +28,19 @@ export  function Input() {
           <IconButton className='chat__additional-btn'>
             <SentimentSatisfiedAltIcon />
           </IconButton >
-          <input className='chat__input' placeholder='Message' type="text" />
+          <input 
+            className='chat__input' 
+            placeholder='Message' 
+            type="text" 
+            value={message} 
+            onChange={e => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown} />
           <IconButton className='chat__additional-btn'>
             <AttachFileIcon />
           </IconButton>
         </div>
       </div>
-        <IconButton className='chat__send-msg-btn'>
+        <IconButton className='chat__send-msg-btn' onClick={sendMessage}>
           <SendIcon />
         </IconButton>
     </div>
