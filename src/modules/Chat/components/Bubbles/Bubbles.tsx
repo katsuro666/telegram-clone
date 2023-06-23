8e084f736c62fc22338c606d447ba4f6794c7a42
katsuro666 <kreepinoutdakut@gmail.com>
2023-06-23 13:26:46 +0300

    add scrollbar when chat messages go offscreen

--- src/modules/Chat/components/Bubbles/Bubbles.tsx
+++ src/modules/Chat/components/Bubbles/Bubbles.tsx
@@ -4,10 +4,37 @@ import './Bubbles.scss'
 export function Bubbles() {
   return (
     <div className='chat__bubbles'>
+      <p>Bubbles1</p>
+      <p>Bubbles</p>
+      <p>Bubbaczxczxcdfgles</p>
+      <p>Bubbsdfles233</p>
+      <p>Bubbsdles</p>
+      <p>Bubbdlxfsfses33231</p>
+      <p>Bub1231bles</p>
+      <p>Bub335346456bles123</p>
+      <p>Bubbles</p>
+      <p>Bubbsdfsles</p>
+      <p>Bubbsddfsles123123</p>
+      <p>Bubbles</p>
+      <p>Bubbsdfles</p>
+      <p>Bubb3123les1312</p>
       <p>Bubbles</p>
       <p>Bubbles</p>
+      <p>Bub312bles</p>
       <p>Bubbles</p>
       <p>Bubbles</p>
+      <p>Bub123123bles</p>
+      <p>Bubbsdfsles</p>
+      <p>Bubbsddfsles123123</p>
+      <p>Bubbles</p>
+      <p>Bubbsdfles</p>
+      <p>Bubb3123les1312</p>
+      <p>Bubbles</p>
+      <p>Bubbles</p>
+      <p>Bub312bles</p>
+      <p>Bubbles</p>
+      <p>Bubbles</p>
+      <p>Bub123123bles</p>
     </div>
   )
 }
