5aac2bc09c3742ac77bb2dd16675fadb67fb4423
katsuro666 <kreepinoutdakut@gmail.com>
2023-08-03 14:58:16 +0300

    now all threads are taken from the database, also add the function of adding a new thread to the database

--- src/modules/Sidebar/Sidebar.jsx
+++ src/modules/Sidebar/Sidebar.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 import { IconButton } from '@mui/material'
 import CreateIcon from '@mui/icons-material/Create';
-
 import { Header } from './components'
 import { Threads } from './components/Threads';
 import { Thread } from './components/Threads/components';
+import { db } from '../../firebase';
 import './Sidebar.scss';
+import { selectUser } from 'features/userSlice';
 
 
 export function Sidebar() {
 
+  const user = useSelector(selectUser)
   const [threads, setThreads] = useState([])
 
+  useEffect(() => {
+    db.collection('threads').onSnapshot((snapshot) => {
+      setThreads(snapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      })))
+    })
+  })
+
   const addThread = async () => {
-    console.log('add thread')
+    const threadName = prompt('Enter a thread name')
+    if (threadName) {
+      db.collection('threads').add({
+        threadName: threadName
+      })
+    }
   }
   
   return (
