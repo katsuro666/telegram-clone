bb2aa26916bf2d6d74b0290450a37055b037ca87
katsuro666 <kreepinoutdakut@gmail.com>
2023-08-24 17:17:42 +0300

    add settings tab

--- src/modules/Sidebar/components/Settings/Settings.jsx
+++ src/modules/Sidebar/components/Settings/Settings.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from 'features/userSlice';
+import './Settings.scss';
+import { Paper, MenuList, MenuItem, ListItemIcon } from '@mui/material';
+import TranslateIcon from '@mui/icons-material/Translate';
 
 export function Settings() {
+  const user = useSelector(selectUser);
+
   return (
-    <div>
+    <>
       <div className='profile-content'>
-        <div className="profile-content__avatar"></div>
+        <div className='profile__avatar'>
+          <img src={user.photo} alt='your avatar' className='profile__img' draggable='false' />
+        </div>
+        <span className='profile__displayname'>{user.displayName}</span>
+        <span className='profile__username'>{`@${user.username}`}</span>
       </div>
-    </div>
+      <Paper className='user-settings'>
+        <MenuList className='user-settings__list'>
+          <MenuItem className='user-settings__item'>
+            <ListItemIcon>
+              <TranslateIcon className='user-settings__icon' />
+            </ListItemIcon>
+            <div className='user-settings__text-item'>
+              <span className='user-settings__text'>Language</span>
+              <span className='user-settings__text--secondary'>English</span>
+            </div>
+          </MenuItem>
+        </MenuList>
+      </Paper>
+    </>
   );
 }
